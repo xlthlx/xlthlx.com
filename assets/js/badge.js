4d1cb960e2e42d2b62cac5743ccbb7b338833860
xlthlx <xlthlx@gmail.com>
2021-02-20 18:42:26 +0000

    Highlight server side

---
+++ assets/js/badge.js
@@ -0,0 +1,295 @@
+"use strict";
+
+(function (global, factory) {
+
+	if (typeof module === "object" && typeof module.exports === "object") {
+
+		module.exports = global.document ?
+			factory(global, true) :
+			function (w) {
+				if (!w.document) {
+					throw new Error("A window with a document is required");
+				}
+				return factory(w);
+			};
+	} else {
+		factory(global);
+	}
+
+
+}(typeof window !== "undefined" ? window : this, function (window, noGlobal) {
+
+	if (typeof highlightJsBadgeAutoLoad !== 'boolean')
+		var highlightJsBadgeAutoLoad = false;
+
+	function highlightJsBadge(opt) {
+		var options = {
+			templateSelector: "#CodeBadgeTemplate",
+			contentSelector: "body",
+			loadDelay: 0,
+			copyIconClass: "fa fa-copy",
+			copyIconContent: "",
+			checkIconClass: "fa fa-check text-success",
+			checkIconContent: "",
+			onBeforeCodeCopied: null
+		};
+
+		function initialize(opt) {
+			Object.assign(options, opt);
+
+			if (document.readyState == 'loading')
+				document.addEventListener("DOMContentLoaded", load);
+			else
+				load();
+		}
+
+
+		function load() {
+			if (options.loadDelay)
+				setTimeout(addCodeBadge, loadDelay);
+			else
+				addCodeBadge();
+		}
+
+		function addCodeBadge() {
+			// first make sure the template exists - if not we embed it
+			if (!document.querySelector(options.templateSelector)) {
+				var node = document.createElement("div");
+				node.innerHTML = getTemplate();
+				var style = node.querySelector("style");
+				var template = node.querySelector(options.templateSelector);
+				document.body.appendChild(style);
+				document.body.appendChild(template);
+			}
+
+			var hudText = document.querySelector(options.templateSelector).innerHTML;
+
+			var $codes = document.querySelectorAll("pre>code.hljs");
+			for (var index = 0; index < $codes.length; index++) {
+				var el = $codes[index];
+				if (el.querySelector(".code-badge"))
+					continue; // already exists
+
+				var lang = "";
+
+				for (var i = 0; i < el.classList.length; i++) {
+					var cl = el.classList[i];
+					// class="hljs language-csharp"
+					if (cl.substr(0, 9) === 'language-') {
+						lang = el.classList[i].replace('language-', '');
+						break;
+					}
+					// class="hljs lang-cs"  // docFx
+					else if (cl.substr(0, 5) === 'lang-') {
+						lang = el.classList[i].replace('lang-', '');
+						break;
+					}
+					// class="kotlin hljs"   (auto detected)
+					if (!lang) {
+						for (var j = 0; j < el.classList.length; j++) {
+							if (el.classList[j] == 'hljs')
+								continue;
+							lang = el.classList[j];
+							break;
+						}
+					}
+				}
+
+				if (lang)
+					lang = lang.toLowerCase();
+				else
+					lang = "text";
+
+				// Language Name overrides so it displays nicer
+				if (lang == "ps")
+					lang = "powershell";
+				else if (lang == "cs")
+					lang = "csharp";
+				else if (lang == "js")
+					lang = "javascript";
+				else if (lang == "ts")
+					lang = "typescript";
+				else if (lang == "fox")
+					lang = "foxpro";
+				else if (lang == "txt")
+					lang = "text"
+
+
+				var html = hudText.replace("{{language}}", lang)
+					.replace("{{copyIconClass}}", options.copyIconClass)
+					.trim();
+
+				// insert the Hud panel
+				var $newHud = document.createElement("div");
+				$newHud.innerHTML = html;
+				$newHud = $newHud.querySelector(".code-badge");
+
+				// make <pre> tag position:relative so positioning keeps pinned right
+				// even with scroll bar scrolled
+				var pre = el.parentElement;
+				pre.classList.add("code-badge-pre")
+
+				if (options.copyIconContent)
+					$newHud.querySelector(".code-badge-copy-icon").innerText = options.copyIconContent;
+
+				pre.insertBefore($newHud, el);
+			}
+
+			var $content = document.querySelector(options.contentSelector);
+
+			// single copy click handler
+			$content.addEventListener("click",
+				function (e) {
+					var $clicked = e.srcElement;
+					if ($clicked.classList.contains("code-badge-copy-icon")) {
+						e.preventDefault();
+						e.cancelBubble = true;
+						copyCodeToClipboard(e);
+					}
+					return false;
+				});
+		}
+
+
+		function copyCodeToClipboard(e) {
+			// walk back up to <pre> tag
+			var $origCode = e.srcElement.parentElement.parentElement.parentElement;
+
+			// select the <code> tag and grab text
+			var $code = $origCode.querySelector("pre>code");
+			var text = $code.textContent || $code.innerText;
+
+			if (options.onBeforeCodeCopied)
+				text = options.onBeforeCodeCopied(text, $code);
+
+			// Create a textblock and assign the text and add to document
+			var el = document.createElement('textarea');
+			el.value = text.trim();
+			document.body.appendChild(el);
+			el.style.display = "block";
+
+			// select the entire textblock
+			if (window.document.documentMode)
+				el.setSelectionRange(0, el.value.length);
+			else
+				el.select();
+
+			// copy to clipboard
+			document.execCommand('copy');
+
+			// clean up element
+			document.body.removeChild(el);
+
+			// show the check icon (copied) briefly
+			swapIcons($origCode);
+		}
+
+		function swapIcons($code) {
+			var copyIcons = options.copyIconClass.split(' ');
+			var checkIcons = options.checkIconClass.split(' ');
+
+			var $fa = $code.querySelector(".code-badge-copy-icon");
+			$fa.innerText = options.checkIconContent;
+
+			for (var i = 0; i < copyIcons.length; i++)
+				$fa.classList.remove(copyIcons[i]);
+
+			for (var i = 0; i < checkIcons.length; i++)
+				$fa.classList.add(checkIcons[i]);
+
+
+			setTimeout(function () {
+				$fa.innerText = options.copyIconContent;
+
+				for (var i = 0; i < checkIcons.length; i++)
+					$fa.classList.remove(checkIcons[i]);
+				for (var i = 0; i < copyIcons.length; i++)
+					$fa.classList.add(copyIcons[i]);
+			}, 2000);
+		}
+
+		function getTemplate() {
+			var stringArray =
+				[
+					"<style>",
+					"@media print {",
+					"   .code-badge { display: none; }",
+					"}",
+					"    .code-badge-pre {",
+					"        position: relative;",
+					"    }",
+					"    .code-badge {",
+					"        display: flex;",
+					"        flex-direction: row;",
+					"        white-space: normal;",
+					"        background: transparent;",
+					"        background: #333;",
+					"        color: white;",
+					"        font-size: 0.875em;",
+					"        opacity: 0.5;",
+					"        transition: opacity linear 0.5s;",
+					"        border-radius: 0 0 0 7px;",
+					"        padding: 5px 8px 5px 8px;",
+					"        position: absolute;",
+					"        right: 0;",
+					"        top: 0;",
+					"    }",
+					"    .code-badge.active {",
+					"        opacity: 0.8;",
+					"    }",
+					"",
+					"    .code-badge:hover {",
+					"        opacity: .95;",
+					"    }",
+					"",
+					"    .code-badge a,",
+					"    .code-badge a:hover {",
+					"        text-decoration: none;",
+					"    }",
+					"",
+					"    .code-badge-language {",
+					"        margin-right: 10px;",
+					"        font-weight: 600;",
+					"        color: goldenrod;",
+					"    }",
+					"    .code-badge-copy-icon {",
+					"        font-size: 1.2em;",
+					"        cursor: pointer;",
+					"        padding: 0 7px;",
+					"        margin-top:2;",
+					"    }",
+					"    .fa.text-success:{ color: limegreen !important }",
+					"</style>",
+					"<div id=\"CodeBadgeTemplate\" style=\"display:none\">",
+					"    <div class=\"code-badge\">",
+					"        <div class=\"code-badge-language\" >{{language}}</div>",
+					"        <div  title=\"Copy to clipboard\">",
+					"            <i class=\"{{copyIconClass}} code-badge-copy-icon\"></i></i></a>",
+					"        </div>",
+					"     </div>",
+					"</div>"
+				];
+
+			var t = "";
+			for (var i = 0; i < stringArray.length; i++)
+				t += stringArray[i] + "\n";
+
+			return t;
+		}
+
+		initialize(opt);
+	}
+
+
+// global reference Window
+	window.highlightJsBadge = highlightJsBadge;
+
+
+// module export
+	if (window.module && window.module.exports)
+		window.module.exports.highlightJsBadge = highlightJsBadge;
+
+	if (highlightJsBadgeAutoLoad)
+		highlightJsBadge();
+
+}));
--- inc/template-functions.php
+++ inc/template-functions.php
@@ -6,6 +6,8 @@
  * @subpackage  Xlthlx
  */
 
+use Highlight\Highlighter;
+
 add_action( 'admin_menu', 'xlt_remove_menu_pages', 999 );
 /**
  * Removes annoying submenus.
@@ -30,4 +32,42 @@ add_filter( 'pre_option_link_manager_enabled', '__return_true' );
 add_filter( 'wpcf7_load_js', '__return_false' );
 add_filter( 'wpcf7_load_css', '__return_false' );
 
+/**
+ * Renders the block type output for given attributes.
+ *
+ * @param array $attributes Optional. Block attributes. Default empty array.
+ * @param string $content Optional. Block content. Default empty string.
+ *
+ * @return string Rendered block type output.
+ * @throws Exception
+ * @since 5.0.0
+ */
+function xlt_render_code( $attributes, string $content ) {
 
+	$hl = new Highlighter();
+	$hl->setAutodetectLanguages( array( 'php', 'javascript', 'html' ) );
+
+	$content = str_replace( array(
+		'<pre class="wp-block-code">',
+		'<code>',
+		'</code>',
+		'</pre>'
+	), '', html_entity_decode( $content ) );
+
+	$highlighted = $hl->highlightAuto( trim( $content ) );
+
+	if ( $highlighted ) {
+		$content = '<pre class="wp-block-code"><code class="hljs ' . $highlighted->language . '">' . $highlighted->value . '</code></pre>';
+		$content = apply_filters( 'the_content', $content );
+	}
+
+	return $content;
+}
+
+function xlt_render_code_block() {
+	register_block_type( 'core/code', array(
+		'render_callback' => 'xlt_render_code',
+	) );
+}
+
+add_action( 'after_setup_theme', 'xlt_render_code_block' );
