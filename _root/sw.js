7a6fbc3906a4c1e963bbbb387f22dc5c63e07163
xlthlx <xlthlx@gmail.com>
2021-03-07 14:08:51 +0000

    Final optimizations

--- _root/sw.js
+++ _root/sw.js
@@ -1,8 +1,5 @@
-var swsource = "/service-worker.js";
-if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register(swsource).then(function (reg) {
-		console.log('ServiceWorker scope: ', reg.scope);
-	}).catch(function (err) {
-		console.log('ServiceWorker registration failed: ', err);
-	});
-}
+window.addEventListener("load", () => {
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker.register("service-worker.js");
+	}
+});
