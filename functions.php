f951200a4e08d950a572603576a1c0a5c0c0b620
xlthlx <xlthlx@gmail.com>
2021-10-13 08:01:34 +0100

    Remove carbon fields

--- functions.php
+++ functions.php
@@ -208,33 +208,3 @@ require_once 'inc/newsletter/newsletter.php';
  * AMP form.
  */
 require_once 'inc/amp-form/amp-form.php';
-
-
-use Carbon_Fields\Block;
-use Carbon_Fields\Field;
-
-Block::make( __( 'My Shiny Gutenberg Block' ) )
-     ->add_fields( array(
-	     Field::make( 'text', 'heading', __( 'Block Heading' ) ),
-	     Field::make( 'image', 'image', __( 'Block Image' ) ),
-	     Field::make( 'rich_text', 'content', __( 'Block Content' ) ),
-     ) )
-     ->set_render_callback( function ( $fields, $attributes, $inner_blocks ) {
-	     ?>
-
-	     <div class="block">
-		     <div class="block__heading">
-			     <h1><?php echo esc_html( $fields['heading'] ); ?></h1>
-		     </div><!-- /.block__heading -->
-
-		     <div class="block__image">
-			     <?php echo wp_get_attachment_image( $fields['image'], 'full' ); ?>
-		     </div><!-- /.block__image -->
-
-		     <div class="block__content">
-			     <?php echo apply_filters( 'the_content', $fields['content'] ); ?>
-		     </div><!-- /.block__content -->
-	     </div><!-- /.block -->
-
-	     <?php
-     } );
