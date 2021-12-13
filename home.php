241ae6a32928e1e2fb0aeb11021379da78b9257f
xlthlx <xlthlx@gmail.com>
2021-12-13 09:38:11 +0000

    Sticky post

--- home.php
+++ home.php
@@ -12,7 +12,26 @@ global $paged;
 if ( ! isset( $paged ) || ! $paged ) {
 	$paged = 1;
 }
-$context['posts'] = new Timber\PostQuery( array( 'paged' => $paged ) );
+
+$sticky = get_option( 'sticky_posts' );
+$first  = array_slice( $sticky, 0, 1 );
+
+$offset = array();
+
+if ( count( $first ) === 1 ) {
+	$context['first_posts'] = new Timber\PostQuery( array( 'post__in' => $first, 'ignore_sticky_posts' => 1 ) );
+	$offset                 = $first;
+} else {
+	$context['first_posts'] = new Timber\PostQuery( array( 'posts_per_page' => 1 ) );
+	$offset[0]              = $context['first_posts'][0]->ID;
+}
+
+
+if ( count( $first ) === 1 ) {
+	$offset = array_slice( $sticky, 0, 3 );
+}
+
+$context['posts'] = new Timber\PostQuery( array( 'post__not_in' => $offset, 'paged' => $paged ) );
 
 $context['category'] = Timber::get_terms( [
 	'taxonomy'   => 'category',
