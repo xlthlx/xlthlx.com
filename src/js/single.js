85b0f39da5ce1e9a6e45f7b92e74ad20fa470392
xlthlx <xlthlx@gmail.com>
2022-04-29 08:59:08 +0100

    Remove print

--- src/js/single.js
+++ src/js/single.js
@@ -9,27 +9,6 @@ function openShare(object, e, name) {
     window.open(href, name, "toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=yes,width=700,height=300,top=200,left=" + (width - 700) / 2);
 }
 
-function setPrint() {
-    const pfHeaderImgUrl = '';
-    const pfHeaderTagline = '';
-    const pfdisableClickToDel = 0;
-    const pfHideImages = 0;
-    const pfImageDisplayStyle = 'block';
-    const pfDisablePDF = 0;
-    const pfDisableEmail = 1;
-    const pfDisablePrint = 1;
-    const pfCustomCSS = '';
-    const pfEncodeImages = 0;
-    const pfShowHiddenContent = 0;
-    const pfBtVersion = '2';
-    (function () {
-        let pf;
-        pf = document.createElement('script');
-        pf.src = '//cdn.printfriendly.com/printfriendly.js';
-        document.getElementsByTagName('head')[0].appendChild(pf);
-    })();
-}
-
 let twitterBtn = document.getElementsByClassName('s-twitter');
 if ( typeof twitterBtn !== 'undefined' && twitterBtn.length > 0 ) {
     twitterBtn[0].addEventListener('click', function (e) {
@@ -41,15 +20,6 @@ let facebookBtn = document.getElementsByClassName('s-facebook');
 if ( typeof facebookBtn !== 'undefined' && twitterBtn.length > 0 ) {
     facebookBtn[0].addEventListener('click', function (e) {
         openShare(this, e, 'fbWindow');
-    });
-}
-
-let printBtn = document.getElementsByClassName('s-print');
-if ( typeof printBtn !== 'undefined' && printBtn.length > 0 ) {
-    setPrint();
-    printBtn[0].addEventListener('click', function (e) {
-        e.preventDefault();
-        window.print();
     });
 }
 
