95c737865016603f099cd7fc4ee5b4c8c3dadf67
xlthlx <xlthlx@gmail.com>
2022-06-14 12:57:45 +0100

    Remove main font

--- parts/head.php
+++ parts/head.php
@@ -5,11 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="author" href="<?php echo get_template_directory_uri();?>/humans.txt"/>
-	<link rel="preload" href="<?php echo get_template_directory_uri();?>/assets/fonts/titillium-web-v6-latin-ext_latin-regular.woff2" as="font" type="font/woff2" crossorigin />
-	<link rel="preload" href="<?php echo get_template_directory_uri();?>/assets/fonts/titillium-web-v6-latin-ext_latin-700.woff2" as="font" type="font/woff2" crossorigin />
-	<link rel="preload" href="<?php echo get_template_directory_uri();?>/assets/fonts/titillium-web-v6-latin-ext_latin-italic.woff2" as="font" type="font/woff2" crossorigin />
 	<link rel="preload" href="<?php echo get_template_directory_uri();?>/assets/fonts/ShadowsIntoLight.woff2" as="font" type="font/woff2" crossorigin />
-	<link rel="preload" href="<?php echo get_template_directory_uri();?>/assets/js/main.min.js?ver=<?php echo filemtime(get_template_directory().'/assets/js/main.min.js');?>" as="script" crossorigin />
+	<link rel="preload" href="<?php echo get_template_directory_uri(); ?>/assets/js/main.min.js?ver=<?php echo filemtime( get_template_directory() . '/assets/js/main.min.js' ); ?>" as="script" crossorigin/>
 	<meta name="theme-color" content="#6667ab"/>
 	<link rel="manifest" href="<?php echo get_template_directory_uri();?>/assets/manifest/manifest.json"/>
 	<?php wp_head(); ?>
