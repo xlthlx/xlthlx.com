0cb27dd912b6bfc821f75c858e719e5ff34616a6
xlthlx <xlthlx@gmail.com>
2022-06-18 17:42:09 +0100

    Webp sidebar images

--- parts/newsletter.php
+++ parts/newsletter.php
@@ -10,16 +10,26 @@
 	</div>
 <?php if ( ( 'en' === $lang ) ) { ?>
 	<div id="xlthlx-fix" class="widget widget_grey widget_block p-4 mb-4 rounded-0">
-		<a title="#FixTheDigitalStatus!" href="https://www.the3million.org.uk/fix-the-digital-status" target="_blank">
-			<img loading="lazy" src="<?php echo get_template_directory_uri(); ?>/assets/img/fix-digital-status.jpg"
-				 class="img-fluid" alt="#FixTheDigitalStatus!" width="300" height="120">
-		</a>
+		<figure class="wp-block-image size-full is-style-default">
+			<a title="#FixTheDigitalStatus!" href="https://www.the3million.org.uk/fix-the-digital-status" target="_blank">
+				<picture>
+					<source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/fix-digital-status.webp" type="image/webp">
+					<source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/fix-digital-status.jpg" type="image/jpeg">
+					<img width="300" height="120" src="<?php echo get_template_directory_uri(); ?>/assets/img/fix-digital-status.jpg" alt="#FixTheDigitalStatus!" class="img-fluid" srcset="<?php echo get_template_directory_uri(); ?>/assets/img/fix-digital-status.jpg 300w, <?php echo get_template_directory_uri(); ?>/assets/img/fix-digital-status-150x60.jpg 150w" sizes="(max-width: 300px) 100vw, 300px">
+				</picture>
+			</a>
+		</figure>
 	</div>
 <?php } else { ?>
 	<div id="xlthlx-exit" class="widget widget_grey widget_block p-4 mb-4 rounded-0">
-		<a title="Sbattèzzati!" href="https://www.sbattezzati.it/" target="_blank">
-			<img loading="lazy" src="<?php echo get_template_directory_uri(); ?>/assets/img/exit.jpg" class="img-fluid"
-				 alt="Sbattèzzati!" width="300" height="78">
-		</a>
+		<figure class="wp-block-image size-full is-style-default">
+			<a title="Sbattèzzati!" href="https://www.sbattezzati.it/" target="_blank">
+			<picture>
+				<source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/exit.webp" type="image/webp">
+				<source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/exit.jpg" type="image/jpeg">
+				<img width="300" height="78" src="<?php echo get_template_directory_uri(); ?>/assets/img/exit.jpg" alt="Sbattèzzati!" class="img-fluid" srcset="<?php echo get_template_directory_uri(); ?>/assets/img/exit.jpg 300w, <?php echo get_template_directory_uri(); ?>/assets/img/exit-150x39.jpg 150w" sizes="(max-width: 300px) 100vw, 300px">
+			</picture>
+			</a>
+		</figure>
 	</div>
 <?php }
