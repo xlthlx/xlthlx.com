f22fc708f5ecea66523010dab5b7f2e966143a99
xlthlx <xlthlx@gmail.com>
2022-06-27 19:43:09 +0100

    Sidebars

--- page.php
+++ page.php
@@ -36,11 +36,7 @@ get_header();
 
 					</div>
 
-					<div class="col-md-4">
-						<aside class="sidebar mt-md-0 mt-4 ps-md-4 ps-0">
-							<?php dynamic_sidebar( 'page_sidebar' ); ?>
-						</aside>
-					</div>
+					<?php get_template_part( 'parts/sidebar-page' ); ?>
 				</div>
 
 			</article>
--- parts/newsletter.php
+++ parts/newsletter.php
@@ -1,7 +1,4 @@
 <?php global $lang; ?>
-	<div class="kb-club">
-		<a target="blank" href="https://512kb.club" title="512KB Club Blue Team"><span class="kb-club-no-bg">512KB Club</span><span class="kb-club-bg">Blue Team</span></a>
-	</div>
 	<div id="xlthlx-newsletter" class="widget widget_grey widget_xlthlx-newsletter p-4 mb-4 rounded-0">
 		<h3 class="h2 pb-2 shadows">Newsletter</h3>
 		<div class="textwidget">
