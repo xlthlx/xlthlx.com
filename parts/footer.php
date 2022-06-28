ca5c34be1e324e1768b083e77f2c5b549ed3b53f
xlthlx <xlthlx@gmail.com>
2022-06-28 08:41:43 +0100

    512kb in footer

--- parts/footer.php
+++ parts/footer.php
@@ -1,5 +1,10 @@
 <?php global $site_url, $site_name; ?>
 <div class="container">
+	<p class="kb-club pe-4 float-end d-inline">
+		<a target="blank" href="https://512kb.club" title="512KB Club Blue Team">
+			<span class="kb-club-no-bg">512KB Club</span><span class="kb-club-bg">Blue Team</span>
+		</a>
+	</p>
 	<p class="navbar-nav float-end d-inline">
 		<a title="Back to top" class="nav-link d-inline p-0" href="#top">
 			<svg style="width:20px;height:20px" aria-label="Arrow up" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path></svg>
