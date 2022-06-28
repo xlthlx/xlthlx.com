bb813d70639a7c20022335bd98356aa6effe2462
xlthlx <xlthlx@gmail.com>
2022-06-28 08:43:27 +0100

    Style for badge

--- parts/footer.php
+++ parts/footer.php
@@ -1,10 +1,5 @@
 <?php global $site_url, $site_name; ?>
 <div class="container">
-	<p class="kb-club pe-4 float-end d-inline">
-		<a target="blank" href="https://512kb.club" title="512KB Club Blue Team">
-			<span class="kb-club-no-bg">512KB Club</span><span class="kb-club-bg">Blue Team</span>
-		</a>
-	</p>
 	<p class="navbar-nav float-end d-inline">
 		<a title="Back to top" class="nav-link d-inline p-0" href="#top">
 			<svg style="width:20px;height:20px" aria-label="Arrow up" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path></svg>
@@ -14,6 +9,11 @@
 		</a>
 		<a title="Feed RSS" class="nav-link d-inline ms-2 p-0" target="_blank" href="<?php echo $site_url; ?>feed/">
 			<svg style="width:20px;height:20px" aria-label="Feed RSS" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M128.081 415.959c0 35.369-28.672 64.041-64.041 64.041S0 451.328 0 415.959s28.672-64.041 64.041-64.041 64.04 28.673 64.04 64.041zm175.66 47.25c-8.354-154.6-132.185-278.587-286.95-286.95C7.656 175.765 0 183.105 0 192.253v48.069c0 8.415 6.49 15.472 14.887 16.018 111.832 7.284 201.473 96.702 208.772 208.772.547 8.397 7.604 14.887 16.018 14.887h48.069c9.149.001 16.489-7.655 15.995-16.79zm144.249.288C439.596 229.677 251.465 40.445 16.503 32.01 7.473 31.686 0 38.981 0 48.016v48.068c0 8.625 6.835 15.645 15.453 15.999 191.179 7.839 344.627 161.316 352.465 352.465.353 8.618 7.373 15.453 15.999 15.453h48.068c9.034-.001 16.329-7.474 16.005-16.504z"></path></svg>
+		</a>
+	</p>
+	<p class="kb-club pe-4 float-end d-inline">
+		<a target="blank" href="https://512kb.club" title="512KB Club Blue Team">
+			<span class="kb-club-no-bg">512KB Club</span><span class="kb-club-bg">Blue Team</span>
 		</a>
 	</p>
 	<p class="navbar-nav d-inline" prefix="dct: https://purl.org/dc/terms/ cc: https://creativecommons.org/ns#">
