ef9f7199f41b7892d628ecc17365c816d4414e08
xlthlx <xlthlx@gmail.com>
2022-07-08 09:13:49 +0100

    Test dark mode

---
+++ src/js/dark-mode.js
@@ -0,0 +1,18 @@
+// Dark mode
+const btn = document.querySelector(".btn-toggle");
+const currentTheme = localStorage.getItem("theme");
+
+if ( currentTheme === "dark" ) {
+	document.body.classList.add("dark-theme");
+}
+
+btn.addEventListener("click", function () {
+	document.body.classList.toggle("dark-theme");
+	let theme = "light";
+
+	if ( document.body.classList.contains("dark-theme") ) {
+		theme = "dark";
+	}
+
+	localStorage.setItem("theme", theme);
+});
--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,5 @@ import '../assets/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.js';
 import 'sw.js';
 import 'badge.js';
 import 'single.js';
+import 'dark-mode.js';
 import '../../wp-includes/js/comment-reply.js';
