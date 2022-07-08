d2010af7512c0e284b06e2d4e360d244c58897f7
xlthlx <xlthlx@gmail.com>
2022-07-08 09:13:49 +0100

    Test dark mode

--- header.php
+++ header.php
@@ -9,6 +9,7 @@ global $lang;
 ?>
 <!doctype html>
 <html lang="<?php echo $lang; ?>" id="top">
+<meta name="color-scheme" content="dark light">
 
 <?php get_template_part( 'parts/head' ); ?>
 
@@ -19,9 +20,9 @@ global $lang;
 <main class="pb-0 mb-0">
 	<a class="visually-hidden" href="#content">Skip to content</a>
 	<div class="container py-5 px-3 px-xl-5 mb-0 box-dashed fill">
-		<?php if(!(is_home() || is_front_page())) { ?>
+		<?php if ( ! ( is_home() || is_front_page() ) ) { ?>
 			<nav class="mb-4" style="--bs-breadcrumb-divider: '';" aria-label="breadcrumb">
-				 <?php xlt_breadcrumbs(); ?>
+				<?php xlt_breadcrumbs(); ?>
 			</nav>
 		<?php } ?>
 
