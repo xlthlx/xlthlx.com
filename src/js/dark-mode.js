6f38c5004c36d5ca8f34de68962da600d3f6da0a
xlthlx <xlthlx@gmail.com>
2022-07-08 19:08:14 +0100

    Toggle

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -4,14 +4,17 @@ const currentTheme = localStorage.getItem("theme");
 
 if ( currentTheme === "dark" ) {
 	document.body.classList.add("dark-theme");
+	btn.innerHTML = 'Light mode';
 }
 
 btn.addEventListener("click", function () {
 	document.body.classList.toggle("dark-theme");
 	let theme = "light";
+	btn.innerHTML = 'Dark mode';
 
 	if ( document.body.classList.contains("dark-theme") ) {
 		theme = "dark";
+		btn.innerHTML = 'Light mode';
 	}
 
 	localStorage.setItem("theme", theme);
