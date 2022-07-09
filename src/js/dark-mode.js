adada31cbe2f4edc5ca5e5f8ead152b519a5357e
xlthlx <xlthlx@gmail.com>
2022-07-09 10:20:31 +0100

    Dark mode

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -4,17 +4,17 @@ const currentTheme = localStorage.getItem("theme");
 
 if ( currentTheme === "dark" ) {
 	document.body.classList.add("dark-theme");
-	btn.innerHTML = 'Light mode';
+	btn.innerHTML = '☼';
 }
 
 btn.addEventListener("click", function () {
 	document.body.classList.toggle("dark-theme");
 	let theme = "light";
-	btn.innerHTML = 'Dark mode';
+	btn.innerHTML = '☾';
 
 	if ( document.body.classList.contains("dark-theme") ) {
 		theme = "dark";
-		btn.innerHTML = 'Light mode';
+		btn.innerHTML = '☼';
 	}
 
 	localStorage.setItem("theme", theme);
