7dcaa28bfe35953f2e675900e8dab19d936efbfb
xlthlx <xlthlx@gmail.com>
2022-07-09 10:20:31 +0100

    Dark mode

--- parts/tabs.php
+++ parts/tabs.php
@@ -1,13 +1,13 @@
 <?php global $lang, $post; ?>
 <div class="col-auto d-flex justify-content-end ms-auto">
-	<button id="btn-toggle" class="btn btn-secondary rounded-0 pe-none">Dark mode</button>
+	<span id="btn-toggle" class="btn-secondary lang pink-hover me-2">☾</span>
 	<?php if ( 'en' === $lang ) { ?>
-		<button title="English" type="button" class="btn btn-secondary rounded-0 pe-none lang active">EN</button>
+		<button title="English" type="button" class="btn btn-secondary pe-none lang active">EN</button>
 		<a href="<?php echo get_url_trans(); ?>" title="Italiano"
-		   class="btn btn-outline-secondary rounded-0 lang pink-hover" role="button">IT</a>
+		   class="btn btn-outline-secondary lang pink-hover" role="button">IT</a>
 	<?php } else { ?>
 		<a href="<?php echo get_url_trans(); ?>" title="English"
-		   class="btn btn-outline-secondary rounded-0 lang pink-hover" role="button">EN</a>
-		<button title="Italiano" type="button" class="btn btn-secondary rounded-0 pe-none lang active">IT</button>
+		   class="btn btn-outline-secondary lang pink-hover" role="button">EN</a>
+		<button title="Italiano" type="button" class="btn btn-secondary pe-none lang active">IT</button>
 	<?php } ?>
 </div>
