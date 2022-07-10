f19304b5d6842667d7c7c9dc11c2caef9c4d7339
xlthlx <xlthlx@gmail.com>
2022-07-10 11:11:54 +0100

    Dark mode

--- parts/tabs.php
+++ parts/tabs.php
@@ -1,6 +1,6 @@
 <?php global $lang, $post; ?>
 <div class="col-auto d-flex justify-content-end ms-auto">
-	<a href="#" title="Dark mode" id="btn-toggle" class="btn btn-secondary lang pink-hover me-2">☾</a>
+	<a href="" title="Dark mode" id="btn-toggle" class="btn btn-secondary lang pink-hover">☾</a>
 	<?php if ( 'en' === $lang ) { ?>
 		<button title="English" type="button" class="btn btn-secondary pe-none lang active">EN</button>
 		<a href="<?php echo get_url_trans(); ?>" title="Italiano"
