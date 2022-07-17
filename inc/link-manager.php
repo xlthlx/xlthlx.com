55b75327650e8ec12722c15e98e02e61f9ce5780
xlthlx <xlthlx@gmail.com>
2022-07-17 14:33:46 +0100

    New post types

--- inc/link-manager.php
+++ inc/link-manager.php
@@ -5,26 +5,26 @@
  * @package  xlthlx
  */
 
-add_action( 'load-link-add.php', 'xlt_add_meta_boxes' );
-add_action( 'load-link.php', 'xlt_add_meta_boxes' );
-add_action( 'load-link-manager.php', 'xlt_setup_columns' );
+add_action( 'load-link-add.php','xlt_add_meta_boxes' );
+add_action( 'load-link.php','xlt_add_meta_boxes' );
+add_action( 'load-link-manager.php','xlt_setup_columns' );
 
 /**
  * Adds meta boxes.
  */
 function xlt_add_meta_boxes() {
-	add_action( 'add_meta_boxes', 'xlt_add_link_meta_boxes' );
-	add_action( 'edit_link', 'xlt_save_link_meta_box' );
+	add_action( 'add_meta_boxes','xlt_add_link_meta_boxes' );
+	add_action( 'edit_link','xlt_save_link_meta_box' );
 }
 
 /**
  * All hooks for custom columns.
  */
 function xlt_setup_columns() {
-	add_filter( 'manage_link-manager_columns', 'xlt_add_remove_link_columns' );
-	add_action( 'manage_link_custom_column', 'xlt_add_link_columns_data', 10, 2 );
-	add_filter( 'manage_link-manager_sortable_columns', 'xlt_register_sortable_columns' );
-	add_filter( 'request', 'xlt_order_by_column_year' );
+	add_filter( 'manage_link-manager_columns','xlt_add_remove_link_columns' );
+	add_action( 'manage_link_custom_column','xlt_add_link_columns_data',10,2 );
+	add_filter( 'manage_link-manager_sortable_columns','xlt_register_sortable_columns' );
+	add_filter( 'request','xlt_order_by_column_year' );
 }
 
 /**
@@ -37,7 +37,7 @@ function xlt_add_remove_link_columns( $link_columns ) {
 	$link_columns['directors']        = 'Regia';
 	$link_columns['starring']         = 'Attori';
 
-	unset( $link_columns['rel'], $link_columns['rating'], $link_columns['visible'] );
+	unset( $link_columns['rel'],$link_columns['rating'],$link_columns['visible'] );
 
 	return $link_columns;
 }
@@ -50,10 +50,10 @@ function xlt_add_remove_link_columns( $link_columns ) {
  *
  * @return void
  */
-function xlt_add_link_columns_data( $column_name, $id ) {
+function xlt_add_link_columns_data( $column_name,$id ) {
 
 	if ( $column_name === 'year' ) {
-		$val = get_post_meta( $id, 'link_year', true );
+		$val = get_post_meta( $id,'link_year',true );
 		if ( empty( $val ) ) {
 			return;
 		}
@@ -62,7 +62,7 @@ function xlt_add_link_columns_data( $column_name, $id ) {
 	}
 
 	if ( $column_name === 'directors' ) {
-		$val = get_post_meta( $id, 'link_directors', true );
+		$val = get_post_meta( $id,'link_directors',true );
 		if ( empty( $val ) ) {
 			return;
 		}
@@ -71,7 +71,7 @@ function xlt_add_link_columns_data( $column_name, $id ) {
 	}
 
 	if ( $column_name === 'starring' ) {
-		$val = get_post_meta( $id, 'link_starring', true );
+		$val = get_post_meta( $id,'link_starring',true );
 		if ( empty( $val ) ) {
 			return;
 		}
@@ -80,7 +80,7 @@ function xlt_add_link_columns_data( $column_name, $id ) {
 	}
 
 	if ( $column_name === 'link_description' ) {
-		$val = get_bookmark_field( 'link_description', $id );
+		$val = get_bookmark_field( 'link_description',$id );
 		if ( empty( $val ) ) {
 			return;
 		}
@@ -113,10 +113,10 @@ function xlt_register_sortable_columns( $columns ) {
  */
 function xlt_order_by_column_year( $vars ) {
 	if ( isset( $vars['orderby'] ) && 'year' === $vars['orderby'] ) {
-		$vars = array_merge( $vars, array(
+		$vars = array_merge( $vars,[
 			'meta_key' => 'year',
 			'orderby'  => 'meta_value'
-		) );
+		] );
 	}
 
 	return $vars;
@@ -141,21 +141,21 @@ function xlt_add_link_meta_boxes() {
 function xlt_render_meta_box() {
 
 	global $link;
-	wp_nonce_field( plugin_basename( __FILE__ ), 'xlt_custom_nonce' );
+	wp_nonce_field( plugin_basename( __FILE__ ),'xlt_custom_nonce' );
 
-	$link_year = get_post_meta( $link->link_id, 'link_year', true );
+	$link_year = get_post_meta( $link->link_id,'link_year',true );
 	echo '<label style="padding-right:10px;vertical-align:top" for="link_year">';
 	echo "Anno";
 	echo '</label> ';
 	echo '<input style="vertical-align:top" type="text" id="link_year" name="link_year" value="' . esc_attr( $link_year ) . '"  />';
 
-	$link_directors = get_post_meta( $link->link_id, 'link_directors', true );
+	$link_directors = get_post_meta( $link->link_id,'link_directors',true );
 	echo '<label style="padding-left:10px;padding-right:10px;vertical-align:top" for="link_directors">';
 	echo "Regia";
 	echo '</label> ';
 	echo '<input style="vertical-align:top" type="text" id="link_directors" name="link_directors" value="' . esc_attr( $link_directors ) . '"  />';
 
-	$link_starring = get_post_meta( $link->link_id, 'link_starring', true );
+	$link_starring = get_post_meta( $link->link_id,'link_starring',true );
 	echo '<label style="padding-left:10px;padding-right:10px;vertical-align:top" for="link_starring">';
 	echo "Attori";
 	echo '</label>';
@@ -178,17 +178,16 @@ function xlt_save_link_meta_box( $post_id ) {
 		return;
 	}
 
-	if ( ! isset( $_POST['xlt_custom_nonce'] ) || ! wp_verify_nonce( $_POST['xlt_custom_nonce'], plugin_basename( __FILE__ ) ) ) {
+	if ( ! isset( $_POST['xlt_custom_nonce'] ) || ! wp_verify_nonce( $_POST['xlt_custom_nonce'],plugin_basename( __FILE__ ) ) ) {
 		return;
 	}
 
 	$link_year = sanitize_text_field( $_POST['link_year'] );
-	update_post_meta( $post_id, 'link_year', $link_year );
-
+	update_post_meta( $post_id,'link_year',$link_year );
 
 	$link_directors = sanitize_text_field( $_POST['link_directors'] );
-	update_post_meta( $post_id, 'link_directors', $link_directors );
+	update_post_meta( $post_id,'link_directors',$link_directors );
 
 	$link_starring = sanitize_text_field( $_POST['link_starring'] );
-	update_post_meta( $post_id, 'link_starring', $link_starring );
+	update_post_meta( $post_id,'link_starring',$link_starring );
 }
