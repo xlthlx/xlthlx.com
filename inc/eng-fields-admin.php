2799c220097086e16ba2b6d3799efd298ae851a1
xlthlx <xlthlx@gmail.com>
2022-07-21 09:39:44 +0100

    English fields

--- inc/eng-fields-admin.php
+++ inc/eng-fields-admin.php
@@ -9,83 +9,58 @@
  * Adds the English fields.
  */
 function xlt_add_metabox() {
-	$cmb_post = new_cmb2_box( array(
+	$cmb_post = new_cmb2_box( [
 		'id'           => 'group_en',
 		'title'        => 'English',
-		'object_types' => array( 'post' ),
+		'object_types' => [ 'post','page','film','tvseries' ],
 		'context'      => 'normal',
 		'priority'     => 'high',
-	) );
+	] );
 
-	$cmb_post->add_field( array(
+	$cmb_post->add_field( [
 		'name'         => 'Titolo',
 		'id'           => 'title_en',
 		'type'         => 'text_medium',
 		'show_in_rest' => WP_REST_Server::ALLMETHODS,
-	) );
+	] );
 
-	$cmb_post->add_field( array(
+	$cmb_post->add_field( [
 		'name'         => '',
 		'id'           => 'content_en',
 		'type'         => 'wysiwyg',
-		'options'      => array(
+		'options'      => [
 			'media_buttons' => false,
-		),
+		],
 		'show_in_rest' => WP_REST_Server::ALLMETHODS,
-	) );
-
-	$cmb_page = new_cmb2_box( array(
-		'id'           => 'group_page_en',
-		'title'        => 'English',
-		'object_types' => array( 'page' ),
-		'context'      => 'normal',
-		'priority'     => 'high',
-	) );
-
-	$cmb_page->add_field( array(
-		'name'         => 'Titolo',
-		'id'           => 'title_en',
-		'type'         => 'text_medium',
-		'show_in_rest' => WP_REST_Server::ALLMETHODS,
-	) );
-
-	$cmb_page->add_field( array(
-		'name'         => '',
-		'id'           => 'content_en',
-		'type'         => 'wysiwyg',
-		'options'      => array(
-			'media_buttons' => false,
-		),
-		'show_in_rest' => WP_REST_Server::ALLMETHODS,
-	) );
+	] );
 }
 
-add_action( 'cmb2_init', 'xlt_add_metabox' );
+add_action( 'cmb2_init','xlt_add_metabox' );
 
 function xlt_add_category_metabox() {
-	$cmb_category = new_cmb2_box( array(
+	$cmb_category = new_cmb2_box( [
 		'id'               => 'category_group_en',
 		'title'            => 'English',
-		'object_types'     => array( 'term' ),
-		'taxonomies'       => array( 'category' ),
+		'object_types'     => [ 'term' ],
+		'taxonomies'       => [ 'category' ],
 		'priority'         => 'high',
 		'show_in_rest'     => WP_REST_Server::ALLMETHODS,
-		'mb_callback_args' => array( '__block_editor_compatible_meta_box' => true ),
-	) );
+		'mb_callback_args' => [ '__block_editor_compatible_meta_box' => true ],
+	] );
 
-	$cmb_category->add_field( array(
+	$cmb_category->add_field( [
 		'id'           => 'category_en',
 		'name'         => 'Name',
 		'type'         => 'text',
-		'column'       => array(
+		'column'       => [
 			'position' => 2,
 			'name'     => 'Name',
-		),
+		],
 		'show_in_rest' => WP_REST_Server::ALLMETHODS,
-	) );
+	] );
 }
 
-add_action( 'cmb2_init', 'xlt_add_category_metabox' );
+add_action( 'cmb2_init','xlt_add_category_metabox' );
 
 /**
  * Add columns in the admin list.
@@ -107,8 +82,8 @@ function xlt_eng_posts_columns( $defaults ) {
 	return $defaults;
 }
 
-add_filter( 'manage_posts_columns', 'xlt_eng_posts_columns', 15 );
-add_filter( 'manage_pages_columns', 'xlt_eng_posts_columns', 15 );
+add_filter( 'manage_posts_columns','xlt_eng_posts_columns',15 );
+add_filter( 'manage_pages_columns','xlt_eng_posts_columns',15 );
 
 /**
  * Sets columns values.
@@ -116,9 +91,9 @@ add_filter( 'manage_pages_columns', 'xlt_eng_posts_columns', 15 );
  * @param string $column_name
  * @param int $id
  */
-function xlt_eng_posts_custom_columns( $column_name, $id ) {
+function xlt_eng_posts_custom_columns( $column_name,$id ) {
 	if ( $column_name === 'eng' ) {
-		$content_en = get_post_meta( $id, 'content_en', true );
+		$content_en = get_post_meta( $id,'content_en',true );
 		if ( $content_en === '' ) {
 			echo 'No';
 		}
@@ -133,36 +108,36 @@ function xlt_eng_posts_custom_columns( $column_name, $id ) {
 	}
 }
 
-add_action( 'manage_posts_custom_column', 'xlt_eng_posts_custom_columns', 15, 2 );
-add_action( 'manage_pages_custom_column', 'xlt_eng_posts_custom_columns', 15, 2 );
+add_action( 'manage_posts_custom_column','xlt_eng_posts_custom_columns',15,2 );
+add_action( 'manage_pages_custom_column','xlt_eng_posts_custom_columns',15,2 );
 
 /**
  * Hook in and register a metabox for the admin comment edit page.
  */
 function xlt_register_comment_language() {
 
-	$cmb = new_cmb2_box( array(
+	$cmb = new_cmb2_box( [
 		'id'           => 'xlt_comment_metabox',
 		'title'        => 'Altre opzioni',
-		'object_types' => array( 'comment' ),
-	) );
+		'object_types' => [ 'comment' ],
+	] );
 
-	$cmb->add_field( array(
+	$cmb->add_field( [
 		'name'             => 'Lingua del commento',
 		'desc'             => '',
 		'id'               => 'comment_lang',
 		'type'             => 'select',
 		'show_option_none' => false,
 		'default'          => 'it',
-		'options'          => array(
+		'options'          => [
 			'it' => 'Italiano',
 			'en' => 'English'
-		),
-		'column'           => array(
+		],
+		'column'           => [
 			'position' => 4,
 			'name'     => 'Lingua',
-		),
-	) );
+		],
+	] );
 }
 
-add_action( 'cmb2_init', 'xlt_register_comment_language' );
+add_action( 'cmb2_init','xlt_register_comment_language' );
