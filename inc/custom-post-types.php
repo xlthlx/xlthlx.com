cd09a36c4548a437e77b91ecb3cc8d147ba8178f
xlthlx <xlthlx@gmail.com>
2022-07-23 12:16:05 +0100

    Link field

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -280,3 +280,21 @@ add_action( 'after_switch_theme','xlt_rewrite_flush' );
 /**
  * Add url custom field.
  */
+function xlt_add_film_metabox() {
+	$cmb_post = new_cmb2_box( [
+		'id'           => 'group_link',
+		'title'        => 'Additional fields',
+		'object_types' => [ 'film','tvseries' ],
+		'context'      => 'side',
+		'priority'     => 'high',
+	] );
+
+	$cmb_post->add_field( [
+		'name'         => 'Link',
+		'id'           => 'link',
+		'type'         => 'text_url',
+		'show_in_rest' => WP_REST_Server::ALLMETHODS,
+	] );
+}
+
+add_action( 'cmb2_init','xlt_add_film_metabox' );
