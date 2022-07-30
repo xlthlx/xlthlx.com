db73cb5dc6cea737efbb259adb4c9b3a87fb35ca
xlthlx <xlthlx@gmail.com>
2022-07-30 15:41:35 +0100

    Hot fix 2

--- inc/toolkit/index.php
+++ inc/toolkit/index.php
@@ -5,7 +5,7 @@
  * @package  xlthlx
  */
 
-$folder = __DIR__ . '/inc/';
+$folder = get_template_directory() . '/inc/toolkit/inc/';
 $files  = list_files( $folder,2 );
 foreach ( $files as $file ) {
 	if ( is_file( $file ) ) {
