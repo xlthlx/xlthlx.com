33b1286c6afa7d28cae069ee834974efe9c2af19
xlthlx <xlthlx@gmail.com>
2022-08-04 09:42:20 +0100

    Fix report issues

--- parts/head.php
+++ parts/head.php
@@ -4,19 +4,20 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="author" href="<?php echo get_template_directory_uri();?>/humans.txt"/>
+	<link rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt"/>
 	<meta name="theme-color" content="#6667ab"/>
-	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/favicon.ico" sizes="any"><!-- 32×32 -->
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/favicon.ico" sizes="any">
 	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/icon.svg" type="image/svg+xml">
-	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/apple-touch-icon.png"><!-- 180×180 -->
-	<link rel="manifest" href="<?php echo get_template_directory_uri();?>/assets/manifest/manifest.json"/>
+	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/apple-touch-icon.png">
+	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/manifest/manifest.json"/>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Shadows+Into+Light&family=Titillium+Web&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 
-	<?php if('http://localhost' !== home_url()) { ?>
-	<script id="stats" defer data-domain="xlthlx.com" data-api="https://plausible.io/api/event" src="/stats/js/script.outbound-links.file-downloads.hash.js"></script>
-	<script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>
+	<?php if ( 'http://localhost' !== home_url() ) { ?>
+		<script id="stats" defer data-domain="xlthlx.com" data-api="https://plausible.io/api/event" src="/stats/js/script.outbound-links.file-downloads.hash.js"></script>
+		<script>window.plausible = window.plausible || function () {
+				(window.plausible.q = window.plausible.q || []).push(arguments)
+			}</script>
 	<?php } ?>
 </head>
