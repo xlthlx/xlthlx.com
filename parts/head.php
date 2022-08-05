75872fb1609ac85b65366c195193c74c59f053cd
xlthlx <xlthlx@gmail.com>
2022-08-05 09:56:22 +0100

    Small fixes

--- parts/head.php
+++ parts/head.php
@@ -11,13 +11,10 @@
 	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/apple-touch-icon.png">
 	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/manifest/manifest.json"/>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<script id="stats" defer data-domain="xlthlx.com" data-api="https://plausible.io/api/event" src="/stats/js/script.outbound-links.file-downloads.hash.js"></script>
+	<script>window.plausible = window.plausible || function () {
+			(window.plausible.q = window.plausible.q || []).push(arguments)
+		}</script>
 	<?php wp_head(); ?>
-
-	<?php if ( 'http://localhost' !== home_url() ) { ?>
-		<script id="stats" defer data-domain="xlthlx.com" data-api="https://plausible.io/api/event" src="/stats/js/script.outbound-links.file-downloads.hash.js"></script>
-		<script>window.plausible = window.plausible || function () {
-				(window.plausible.q = window.plausible.q || []).push(arguments)
-			}</script>
-	<?php } ?>
 </head>
