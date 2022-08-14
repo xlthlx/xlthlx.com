01a4acef06da732d9d0e9f7f3736d3496a076109
xlthlx <xlthlx@gmail.com>
2022-08-14 09:24:37 +0100

    Better arrows

--- parts/tease-home.php
+++ parts/tease-home.php
@@ -13,11 +13,8 @@ $more = ( 'en' === $lang ) ? "Keep reading: '" : "Continua a leggere: '";
 				<p class="card-text mb-auto me-5"><?php echo xlt_get_excerpt( 20 ); ?>
 					<a aria-label="<?php echo $more . get_the_title(); ?>" href="<?php echo get_the_permalink(); ?>"
 					   class="display-6 stretched-link text-decoration-none arrow ln-0 align-middle">
-						<svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" x="0px" y="0px" viewBox="0 0 512 512">
-							<path fill="currentColor" d="M505.183,239.544L388.819,123.179c-6.654-6.658-16.668-8.645-25.363-5.046
-	c-8.696,3.603-14.367,12.089-14.367,21.501v93.092H23.273C10.42,232.727,0,243.147,0,256s10.42,23.273,23.273,23.273h325.818v93.091
-	c0,9.413,5.669,17.9,14.367,21.501c2.878,1.193,5.904,1.773,8.901,1.773c6.056,0,12.009-2.365,16.46-6.819l116.364-116.364
-	C514.273,263.368,514.273,248.633,505.183,239.544z"/>
+						<svg fill="currentColor" style="width:20px;height:20px" aria-label="<?php echo $more . get_the_title(); ?>" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="-32 0 512 512">
+							<path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"/>
 						</svg>
 					</a>
 				</p>
