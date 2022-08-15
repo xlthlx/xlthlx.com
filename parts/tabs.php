b515b4ce07f997d47f97eb938b7ac0bb899c9d12
xlthlx <xlthlx@gmail.com>
2022-08-15 09:31:41 +0100

    Better buttons

--- parts/tabs.php
+++ parts/tabs.php
@@ -1,6 +1,6 @@
 <?php global $lang,$post,$site_url; ?>
 <div class="col-auto d-flex justify-content-end ms-auto">
-	<a href="<?php echo $site_url; ?>mode/" title="Dark mode" id="btn-toggle" class="btn btn-outline-secondary lang pink-hover">
+	<a href="<?php echo $site_url; ?>mode/" title="Dark mode" id="btn-toggle" class="btn btn-outline-secondary lang pink-hover white-border">
 		<svg style="margin-top:-6px" aria-label="Dark mode" role="img" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
 			<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
 		</svg>
