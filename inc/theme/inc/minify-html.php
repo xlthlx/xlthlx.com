b9f4095fafd30961b68de6951cdb1fa4b999ae06
xlthlx <xlthlx@gmail.com>
2022-09-04 00:25:36 +0200

    Minify HTML original

--- inc/theme/inc/minify-html.php
+++ inc/theme/inc/minify-html.php
@@ -2,26 +2,28 @@
 /**
  * Minify HTML.
  *
- * @package  xlthlx
+ * @package  WordPress
+ * @subpackage  Xlthlx
  */
 
 function xlt_init_minify_html() {
 	ob_start( 'xlt_minify_html_output' );
 }
 
-if ( ! ( wp_doing_cron() ) && ! ( defined( 'WP_CLI' ) && WP_CLI ) && ! is_admin() && ! is_user_logged_in() ) {
-	add_action( 'init','xlt_init_minify_html',1 );
+if ( ! ( defined( 'WP_CLI' ) && WP_CLI ) && ! is_admin() && ! is_user_logged_in() ) {
+	add_action( 'init', 'xlt_init_minify_html', 1 );
 }
 
 function xlt_minify_html_output( $buffer ) {
-	if ( 0 === strpos( ltrim( $buffer ),'<?xml' ) ) {
+	if ( 0 === strpos( ltrim( $buffer ), '<?xml' ) ) {
 		return ( $buffer );
 	}
 
 	$mod = '/u';
 
-	$buffer = str_replace( [ chr( 13 ),chr( 9 ) ],[ chr( 10 ),'' ],$buffer );
-	$buffer = str_ireplace( [
+	$buffer = str_replace( array( chr( 13 ) . chr( 10 ), chr( 9 ) ),
+		array( chr( 10 ), '' ), $buffer );
+	$buffer = str_ireplace( array(
 		'<script',
 		'/script>',
 		'<pre',
@@ -30,7 +32,7 @@ function xlt_minify_html_output( $buffer ) {
 		'/textarea>',
 		'<style',
 		'/style>'
-	],[
+	), array(
 		'M1N1FY-ST4RT<script',
 		'/script>M1N1FY-3ND',
 		'M1N1FY-ST4RT<pre',
@@ -39,20 +41,20 @@ function xlt_minify_html_output( $buffer ) {
 		'/textarea>M1N1FY-3ND',
 		'M1N1FY-ST4RT<style',
 		'/style>M1N1FY-3ND'
-	],$buffer );
-	$split  = explode( 'M1N1FY-3ND',$buffer );
+	), $buffer );
+	$split  = explode( 'M1N1FY-3ND', $buffer );
 	$buffer = '';
 	foreach ( $split as $iValue ) {
-		$ii = strpos( $iValue,'M1N1FY-ST4RT' );
+		$ii = strpos( $iValue, 'M1N1FY-ST4RT' );
 		if ( $ii !== false ) {
-			$process = substr( $iValue,0,$ii );
-			$asis    = substr( $iValue,$ii + 12 );
-			if ( 0 === strpos( $asis,'<script' ) ) {
-				$split2 = explode( chr( 10 ),$asis );
+			$process = substr( $iValue, 0, $ii );
+			$asis    = substr( $iValue, $ii + 12 );
+			if ( 0 === strpos( $asis, '<script' ) ) {
+				$split2 = explode( chr( 10 ), $asis );
 				$asis   = '';
 				foreach ( $split2 as $iiiValue ) {
 					if ( $iiiValue ) {
-						$asis .= trim( $iiiValue );
+						$asis .= trim( $iiiValue ) . chr( 10 );
 					}
 					if ( strpos( $iiiValue,
 							'//' ) !== false && substr( trim( $iiiValue ),
@@ -61,30 +63,30 @@ function xlt_minify_html_output( $buffer ) {
 					}
 				}
 				if ( $asis ) {
-					$asis = substr( $asis,0,- 1 );
+					$asis = substr( $asis, 0, - 1 );
 				}
-				$asis = preg_replace( '!/\*[^*]*\*+([^/][^*]*\*+)*/!','',
+				$asis = preg_replace( '!/\*[^*]*\*+([^/][^*]*\*+)*/!', '',
 					$asis );
 
-				$asis = str_replace( [
+				$asis = str_replace( array(
 					';' . chr( 10 ),
 					'>' . chr( 10 ),
 					'{' . chr( 10 ),
 					'}' . chr( 10 ),
 					',' . chr( 10 )
-				],[ ';','>','{','}',',' ],$asis );
+				), array( ';', '>', '{', '}', ',' ), $asis );
 
-			} elseif ( 0 === strpos( $asis,'<style' ) ) {
-				$asis = preg_replace( [
+			} elseif ( 0 === strpos( $asis, '<style' ) ) {
+				$asis = preg_replace( array(
 					'/\>[^\S ]+' . $mod,
 					'/[^\S ]+\<' . $mod,
 					'/(\s)+' . $mod
-				],[ '>','<','\\1' ],$asis );
+				), array( '>', '<', '\\1' ), $asis );
 
-				$asis = preg_replace( '!/\*[^*]*\*+([^/][^*]*\*+)*/!','',
+				$asis = preg_replace( '!/\*[^*]*\*+([^/][^*]*\*+)*/!', '',
 					$asis );
 
-				$asis = str_replace( [
+				$asis = str_replace( array(
 					chr( 10 ),
 					' {',
 					'{ ',
@@ -99,7 +101,7 @@ function xlt_minify_html_output( $buffer ) {
 					' ,',
 					', ',
 					';}'
-				],[
+				), array(
 					'',
 					'{',
 					'{',
@@ -114,34 +116,40 @@ function xlt_minify_html_output( $buffer ) {
 					',',
 					',',
 					'}'
-				],$asis );
+				), $asis );
 			}
 		} else {
 			$process = $iValue;
 			$asis    = '';
 		}
-		$process = preg_replace( [
+		$process = preg_replace( array(
 			'/\>[^\S ]+' . $mod,
 			'/[^\S ]+\<' . $mod,
 			'/(\s)+' . $mod
-		],[ '>','<','\\1' ],$process );
+		), array( '>', '<', '\\1' ), $process );
 
 		$process = preg_replace( '/<!--(?!\s*(?:\[if [^\]]+]|<!|>))(?:(?!-->).)*-->' . $mod,
-			'',$process );
+			'', $process );
 
 		$buffer .= $process . $asis;
 	}
-	$buffer = str_replace( [
+	$buffer = str_replace( array(
 		chr( 10 ) . '<script',
 		chr( 10 ) . '<style',
 		'*/' . chr( 10 ),
 		'M1N1FY-ST4RT'
-	],[ '<script','<style','*/','' ],$buffer );
+	), array( '<script', '<style', '*/', '' ), $buffer );
 
 	if ( 0 === stripos( ltrim( $buffer ),
 			'<!doctype html>' ) ) {
-		$buffer = str_replace( ' />','>',$buffer );
+		$buffer = str_replace( ' />', '>', $buffer );
 	}
+
+	$buffer = str_replace( array(
+		'https://' . $_SERVER['HTTP_HOST'] . '/',
+		'http://' . $_SERVER['HTTP_HOST'] . '/',
+		'//' . $_SERVER['HTTP_HOST'] . '/'
+	), array( '/', '/', '/' ), $buffer );
 
 	return ( $buffer );
 }
