f04078797f789a812563c263554143019e6cc928
Serena Piccioni <xlthlx@gmail.com>
2022-09-03 10:09:49 +0200

    Update index.php

--- inc/toolkit/index.php
+++ inc/toolkit/index.php
@@ -11,6 +11,6 @@ $folder = get_template_directory() . '/inc/toolkit/inc/';
 $files  = list_files( $folder,2 );
 foreach ( $files as $file ) {
 	if ( is_file( $file ) ) {
-		require_once $file;
+		//require_once $file;
 	}
 }
