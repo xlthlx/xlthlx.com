5ebf9bfa6e05537d75490be926938d00c7bb92ad
xlthlx <xlthlx@gmail.com>
2022-09-13 08:42:44 +0100

    Fix TV Series page

--- page-tv.php
+++ page-tv.php
@@ -42,7 +42,7 @@ get_header();
 									}
 
 									echo '<p>';
-									echo '<h3><a title="' . $serie['title'] . '" target="_blank" href="' . $serie['link'] . '">' . $serie['title'] . ' svg width="20px" height="20px" class="open-new" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M12.1.6a.944.944 0 0 0 .2 1.04l1.352 1.353L10.28 6.37a.956.956 0 0 0 1.35 1.35l3.382-3.38 1.352 1.352a.944.944 0 0 0 1.04.2.958.958 0 0 0 .596-.875V.96a.964.964 0 0 0-.96-.96h-4.057a.958.958 0 0 0-.883.6z"/><path fill="currentColor" d="M14 11v5a2.006 2.006 0 0 1-2 2H2a2.006 2.006 0 0 1-2-2V6a2.006 2.006 0 0 1 2-2h5a1 1 0 0 1 0 2H2v10h10v-5a1 1 0 0 1 2 0z"/></svg></a></h3>';
+									echo '<h3><a title="' . $serie['title'] . '" target="_blank" href="' . $serie['link'] . '">' . $serie['title'] . ' <svg width="20px" height="20px" class="open-new" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M12.1.6a.944.944 0 0 0 .2 1.04l1.352 1.353L10.28 6.37a.956.956 0 0 0 1.35 1.35l3.382-3.38 1.352 1.352a.944.944 0 0 0 1.04.2.958.958 0 0 0 .596-.875V.96a.964.964 0 0 0-.96-.96h-4.057a.958.958 0 0 0-.883.6z"/><path fill="currentColor" d="M14 11v5a2.006 2.006 0 0 1-2 2H2a2.006 2.006 0 0 1-2-2V6a2.006 2.006 0 0 1 2-2h5a1 1 0 0 1 0 2H2v10h10v-5a1 1 0 0 1 2 0z"/></svg></a></h3>';
 									echo '<em><strong>' . $serie['year'] . '</strong></em>';
 
 									if ( $serie['directors'] ) {
