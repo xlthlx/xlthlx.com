f601eb96a5895bb5f17a4a04d5c3fc342774709e
xlthlx <xlthlx@gmail.com>
2022-10-29 11:19:46 +0100

    Move language nav

--- header.php
+++ header.php
@@ -19,7 +19,18 @@ global $lang;
 </header>
 <main class="pb-0 mb-0">
 	<a class="visually-hidden" href="#content">Skip to content</a>
-	<div class="container py-5 px-3 px-xl-5 mb-0 box-dashed fill">
+	<div class="container pb-5 px-3 px-xl-5 mb-0 box-dashed fill">
+		<div class="nav pt-3 mb-3">
+			<?php if ( 'en' === $lang ) { ?>
+				<button title="English" type="button" class="btn btn-secondary pe-none lang active">EN</button>
+				<a href="<?php echo get_url_trans(); ?>" title="Italiano"
+				   class="btn btn-outline-secondary lang pink-hover" role="button">IT</a>
+			<?php } else { ?>
+				<button title="Italiano" type="button" class="btn btn-secondary pe-none lang active">IT</button>
+				<a href="<?php echo get_url_trans(); ?>" title="English"
+				   class="btn btn-outline-secondary lang pink-hover" role="button">EN</a>
+			<?php } ?>
+		</div>
 		<?php if ( ! ( is_home() || is_front_page() ) ) { ?>
 			<nav class="mb-4" style="--bs-breadcrumb-divider: '';" aria-label="breadcrumb">
 				<?php xlt_breadcrumbs(); ?>
--- parts/tabs.php
+++ parts/tabs.php
@@ -5,13 +5,4 @@
 			<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
 		</svg>
 	</a>
-	<?php if ( 'en' === $lang ) { ?>
-		<button title="English" type="button" class="btn btn-secondary pe-none lang active">EN</button>
-		<a href="<?php echo get_url_trans(); ?>" title="Italiano"
-		   class="btn btn-outline-secondary lang pink-hover" role="button">IT</a>
-	<?php } else { ?>
-		<a href="<?php echo get_url_trans(); ?>" title="English"
-		   class="btn btn-outline-secondary lang pink-hover" role="button">EN</a>
-		<button title="Italiano" type="button" class="btn btn-secondary pe-none lang active">IT</button>
-	<?php } ?>
 </div>
