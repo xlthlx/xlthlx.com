53e7dcfe9208bec67c92c7c6d8c6b4e6c4b25ba4
xlthlx <xlthlx@gmail.com>
2022-10-30 12:52:20 +0000

    New navbar

--- parts/navbar.php
+++ parts/navbar.php
@@ -6,33 +6,24 @@
 			   href="<?php echo $site_url; ?>"><?php echo $site_name; ?></a>
 		</h1>
 
-		<?php get_template_part( 'parts/tabs' ); ?>
-
-		<button id="navbar-toggler" class="navbar-toggler text-white border-0 shadow-none pe-3"
-				type="button" data-bs-toggle="offcanvas" data-bs-target="#menuOffcanvas" aria-expanded="false"
-				aria-controls="menuOffcanvas" aria-label="{{ _e( 'Toggle navigation') }}">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-
-		<div class="offcanvas offcanvas-end bg-dark" tabindex="-1" id="menuOffcanvas" aria-labelledby="menuOffcanvasLabel">
-			<div class="offcanvas-header pb-0 white-text">
-				<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close">
-				</button>
-			</div>
-			<div class="offcanvas-body pb-5rem px-5">
-				<ul class="navbar-nav me-auto mb-2 mb-xl-0">
-
+		<nav class="navbar navbar-expand-lg">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarToggler">
+				<?php get_template_part( 'parts/tabs' ); ?>
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0 pe-4">
 					<?php
 					$menu_items = xlt_get_menu_items( 'primary' );
-					foreach ( $menu_items as $menu_item ) { 
+					foreach ( $menu_items as $menu_item ) {
 						?>
 						<?php if ( isset( $menu_item['submenu'] ) ) { ?>
-							<li class="nav-item dropdown pink<?php echo $menu_item['classes']; ?>">
-								<a class="nav-link dropdown-toggle pink show"<?php echo $menu_item['target']; ?>
+							<li class="nav-item dropdown<?php echo $menu_item['classes']; ?>">
+								<a class="nav-link dropdown-toggle"<?php echo $menu_item['target']; ?>
 								   href="<?php echo $menu_item['url']; ?>"
 								   id="<?php echo sanitize_title( $menu_item['title'] ); ?>" data-bs-toggle="dropdown"
 								   aria-expanded="true"><?php echo $menu_item['title']; ?></a>
-								<ul class="dropdown-menu bg-dark border-0 rounded-0 show"
+								<ul class="dropdown-menu bg-dark border-0 rounded-0"
 									aria-labelledby="<?php echo sanitize_title( $menu_item['title'] ); ?>">
 									<?php foreach ( $menu_item['submenu'] as $menu_subitem ) { ?>
 										<li class="white">
@@ -58,7 +49,7 @@
 					<?php get_template_part( 'parts/search-form' ); ?>
 				</div>
 			</div>
-		</div>
+		</nav>
 	</div>
 </nav>
 
