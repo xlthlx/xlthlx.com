a13f99b524f814ae96fac3df0f5d2a06bebe3ad2
xlthlx <xlthlx@gmail.com>
2022-11-02 22:19:43 +0000

    Search form

--- parts/search-form.php
+++ parts/search-form.php
@@ -4,12 +4,11 @@ $label = ( 'en' === $lang ) ? 'Search' : 'Cerca'; ?>
 	<label for="s" class="visually-hidden"><?php echo $label; ?></label>
 	<input class="form-control me-2 rounded-0" type="text" aria-label="<?php echo $label; ?>" name="s" id="s"
 		   placeholder="<?php echo $label; ?>">
-	<?php 
-	if ( 'en' === $lang ) {
-		?>
-		<input type="hidden" name="lang" id="lang" value="en"><?php } ?>
-	<button class="btn btn-sm btn-dark rounded-0 border-0" type="submit">
-		<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"
+	<?php if ( 'en' === $lang ) { ?>
+		<input type="hidden" name="lang" id="lang" value="en">
+	<?php } ?>
+	<button class="btn btn-dark rounded-0 border-0 btn-50" type="submit">
+		<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none"
 			 stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
 			 stroke-width="2" role="img" viewBox="0 0 24 24" focusable="false"><title><?php echo $label; ?></title>
 			<circle cx="10.5" cy="10.5" r="7.5"/>
