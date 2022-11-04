221e84caf4f2c0e07e7452aaaebaeaac53e932aa
xlthlx <xlthlx@gmail.com>
2022-11-04 09:20:06 +0000

    Buttons position

--- header.php
+++ header.php
@@ -22,13 +22,13 @@ global $lang, $site_url;
 	<div class="container pb-5 px-3 px-xl-5 mb-0 box-dashed fill">
 		<div class="nav pt-3 mb-3">
 			<?php if ( 'en' === $lang ) { ?>
-				<button title="English" type="button" class="btn btn-secondary pe-none lang active">EN</button>
 				<a href="<?php echo get_url_trans(); ?>" title="Italiano"
 				   class="btn btn-outline-secondary lang pink-hover" role="button">IT</a>
+				<button title="English" type="button" class="btn btn-secondary pe-none lang active">EN</button>
 			<?php } else { ?>
+				<button title="Italiano" type="button" class="btn btn-secondary pe-none lang active">IT</button>
 				<a href="<?php echo get_url_trans(); ?>" title="English"
 				   class="btn btn-outline-secondary lang pink-hover" role="button">EN</a>
-				<button title="Italiano" type="button" class="btn btn-secondary pe-none lang active">IT</button>
 			<?php } ?>
 			<a href="<?php echo $site_url; ?>mode/" title="Dark mode" id="btn-toggle" class="btn btn-outline-secondary lang pink-hover">
 				<svg class="dark-mode" aria-label="Dark mode" role="img" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" focusable="false">
