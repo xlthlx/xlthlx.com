e51465edc37e1572ebca59e860c9a741036508ce
xlthlx <xlthlx@gmail.com>
2022-11-04 09:18:49 +0000

    Buttons again

--- header.php
+++ header.php
@@ -26,9 +26,9 @@ global $lang, $site_url;
 				<a href="<?php echo get_url_trans(); ?>" title="Italiano"
 				   class="btn btn-outline-secondary lang pink-hover" role="button">IT</a>
 			<?php } else { ?>
-				<button title="Italiano" type="button" class="btn btn-secondary pe-none lang active">IT</button>
 				<a href="<?php echo get_url_trans(); ?>" title="English"
 				   class="btn btn-outline-secondary lang pink-hover" role="button">EN</a>
+				<button title="Italiano" type="button" class="btn btn-secondary pe-none lang active">IT</button>
 			<?php } ?>
 			<a href="<?php echo $site_url; ?>mode/" title="Dark mode" id="btn-toggle" class="btn btn-outline-secondary lang pink-hover">
 				<svg class="dark-mode" aria-label="Dark mode" role="img" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" focusable="false">
