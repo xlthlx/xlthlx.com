4c6d7b3a064f19b5d6fb1875a9d16ff8aa934045
xlthlx <xlthlx@gmail.com>
2022-11-08 20:43:47 +0000

    Sidebars

--- parts/sidebar-post.php
+++ parts/sidebar-post.php
@@ -2,6 +2,7 @@
 	<aside class="sidebar mt-md-0 mt-4 ps-md-4 ps-0">
 		<?php get_template_part( 'parts/newsletter' ); ?>
 		<?php get_template_part( 'parts/buy-me-a-coffee' ); ?>
+		<?php get_template_part( 'parts/sidebar-image' ); ?>
 		<?php dynamic_sidebar( 'sidebar' ); ?>
 	</aside>
 </div>
