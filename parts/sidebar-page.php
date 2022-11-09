be0757fe808d5cefa63a8c7bce8f3d04d050ae77
xlthlx <xlthlx@gmail.com>
2022-11-09 19:08:58 +0000

    Sidebar

--- parts/sidebar-page.php
+++ parts/sidebar-page.php
@@ -1,5 +1,5 @@
-<div class="col-md-4">
-	<aside class="sidebar mt-md-0 mt-4 ps-md-4 ps-0">
+<div class="col-md-3">
+	<aside class="sidebar mt-md-0 mt-4 ps-md-3 ps-0">
 		<?php get_template_part( 'parts/buy-me-a-coffee' ); ?>
 		<?php dynamic_sidebar( 'page_sidebar' ); ?>
 	</aside>
--- parts/sidebar-post.php
+++ parts/sidebar-post.php
@@ -1,5 +1,5 @@
 <div class="col-md-3">
-	<aside class="sidebar mt-md-0 mt-4 ps-md-4 ps-0">
+	<aside class="sidebar mt-md-0 mt-4 ps-md-3 ps-0">
 		<?php get_template_part( 'parts/newsletter' ); ?>
 		<?php get_template_part( 'parts/buy-me-a-coffee' ); ?>
 		<?php get_template_part( 'parts/sidebar-image' ); ?>
