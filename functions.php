04ca9cbb54e9f631a3de9aa66dfd391a42aab9ac
Serena Piccioni <xlthlx@gmail.com>
2022-12-15 15:00:35 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@
 require_once dirname( __FILE__ ) . '/vendor.phar';
 
 add_filter( 'login_display_language_dropdown', '__return_false' );
+add_filter( 'wpcf7_load_js', '__return_false' );
 
 /**
  * Set theme supports and image sizes.
