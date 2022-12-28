86b7ac56975d48d6bf96ff316615949f19c28b15
xlthlx <xlthlx@gmail.com>
2022-12-28 17:33:14 +0000

    Test order by year

--- inc/film-tv/inc/tags.php
+++ inc/film-tv/inc/tags.php
@@ -43,72 +43,67 @@ if ( ! function_exists( 'xlt_get_all_film_tv' ) ) {
 		$output = array();
 		$i      = 0;
 
-		$years = get_terms(
-			array(
-				'taxonomy' => 'year',
-				'orderby'  => 'name',
-				'order'    => 'DESC',
-			)
+
+		$args = array(
+			'post_type'      => $post_type,
+			'posts_per_page' => - 1,
+			'tax_query'      => array(
+				array(
+					'taxonomy' => 'year',
+				),
+			),
 		);
 
-		if ( ! empty( $years ) && ! is_wp_error( $years ) ) {
-			foreach ( $years as $year ) {
-				$args = array(
-					'post_type' => $post_type,
-					'tax_query' => array(
-						array(
-							'taxonomy' => 'year',
-							'field'    => 'slug',
-							'terms'    => array( $year->slug ),
-							'operator' => 'IN',
-						),
-					),
-				);
 
-				$the_query = new WP_Query( $args );
+		$the_query = new WP_Query( $args );
 
-				if ( $the_query->have_posts() ) {
+		if ( $the_query->have_posts() ) {
 
-					while ( $the_query->have_posts() ) {
-						$the_query->the_post();
+			while ( $the_query->have_posts() ) {
+				$the_query->the_post();
 
-						$output[ $i ]['image'] = false;
+				$output[ $i ]['image'] = false;
 
-						if ( get_post_thumbnail_id( get_the_ID() ) ) {
-							$output[ $i ]['image'] = xlt_get_thumb_img( get_post_thumbnail_id( get_the_ID() ), get_the_title() );
-						}
-
-						$output[ $i ]['title']    = get_the_title();
-						$output[ $i ]['link']     = ( 'en' === $lang ) ? get_post_meta( get_the_ID(), 'link_en', true ) : get_post_meta( get_the_ID(), 'link', true );
-						$output[ $i ]['year']     = $year->name;
-						$output[ $i ]['internal'] = get_post_meta( get_the_ID(), 'internal', true );
-
-						$directors                 = get_the_terms( get_the_ID(), 'director' );
-						$output[ $i ]['directors'] = false;
-
-						if ( ! empty( $directors ) && ! is_wp_error( $directors ) ) {
-							$directors                 = wp_list_pluck( $directors, 'name' );
-							$output[ $i ]['directors'] = implode( ', ', $directors );
-						}
-
-						$starring                 = get_the_terms( get_the_ID(), 'actor' );
-						$output[ $i ]['starring'] = false;
-
-						if ( ! empty( $starring ) && ! is_wp_error( $starring ) ) {
-							$starring                 = wp_list_pluck( $starring, 'name' );
-							$output[ $i ]['starring'] = implode( ', ', $starring );
-						}
-
-						$output[ $i ]['content'] = ( 'en' === $lang ) ? get_content_en() : apply_filters( 'the_content', get_the_content() );
-
-						$i ++;
-					}
+				if ( get_post_thumbnail_id( get_the_ID() ) ) {
+					$output[ $i ]['image'] = xlt_get_thumb_img( get_post_thumbnail_id( get_the_ID() ), get_the_title() );
 				}
 
-				wp_reset_postdata();
+				$output[ $i ]['title']    = get_the_title();
+				$output[ $i ]['link']     = ( 'en' === $lang ) ? get_post_meta( get_the_ID(), 'link_en', true ) : get_post_meta( get_the_ID(), 'link', true );
+				$output[ $i ]['internal'] = get_post_meta( get_the_ID(), 'internal', true );
+
+				$years                = get_the_terms( get_the_ID(), 'year' );
+				$output[ $i ]['year'] = false;
+
+				if ( ! empty( $years ) && ! is_wp_error( $years ) ) {
+					$years                = wp_list_pluck( $years, 'name' );
+					$output[ $i ]['year'] = implode( ', ', $years );
+				}
+
+				$directors                 = get_the_terms( get_the_ID(), 'director' );
+				$output[ $i ]['directors'] = false;
+
+				if ( ! empty( $directors ) && ! is_wp_error( $directors ) ) {
+					$directors                 = wp_list_pluck( $directors, 'name' );
+					$output[ $i ]['directors'] = implode( ', ', $directors );
+				}
+
+				$starring                 = get_the_terms( get_the_ID(), 'actor' );
+				$output[ $i ]['starring'] = false;
+
+				if ( ! empty( $starring ) && ! is_wp_error( $starring ) ) {
+					$starring                 = wp_list_pluck( $starring, 'name' );
+					$output[ $i ]['starring'] = implode( ', ', $starring );
+				}
+
+				$output[ $i ]['content'] = ( 'en' === $lang ) ? get_content_en() : apply_filters( 'the_content', get_the_content() );
+
+				$i ++;
 			}
 		}
 
+		wp_reset_postdata();
 		return $output;
+
 	}
 }
