8124d4d057ab1be3ec088245aa8706e086dd16c7
xlthlx <xlthlx@gmail.com>
2023-01-28 17:38:36 +0000

    Fix last modified header format

--- inc/toolkit/inc/last-modified.php
+++ inc/toolkit/inc/last-modified.php
@@ -12,7 +12,7 @@ function xlt_last_mod_header() {
 	if ( is_singular() ) {
 		$post_id = get_queried_object_id();
 		if ( $post_id ) {
-			header( 'Last-Modified: ' . get_the_modified_time( 'D, d M Y H:i:s', $post_id ) );
+			header( 'Last-Modified: ' . get_the_modified_time( 'D, d M Y H:i:s', $post_id ) . 'GMT' );
 		}
 	}
 }
