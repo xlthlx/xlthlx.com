f3a96cfbf9e5c728ee67671f9f5afc00d9646d0f
xlthlx <xlthlx@gmail.com>
2023-02-15 08:56:52 +0000

    Hover state for footer links

--- parts/footer.php
+++ parts/footer.php
@@ -9,11 +9,11 @@ global $site_url, $site_name; ?>
 <div class="container">
 	<div class="row">
 		<div class="navbar-nav col-md-4 d-inline" prefix="dct: https://purl.org/dc/terms/ cc: https://creativecommons.org/ns#">
-				<a class="nav-link d-inline p-2" title="by <?php echo $site_name; ?>"
+				<a class="nav-link d-inline p-2 footer-hover" title="by <?php echo $site_name; ?>"
 				   href="<?php echo $site_url; ?>"
 				   property="cc:attributionName"
 				   rel="cc:attributionURL"><?php echo $site_name; ?></a>
-				<a class="nav-link d-inline p-0" rel="license noreferrer noopener"
+				<a class="nav-link d-inline p-0 footer-hover" rel="license noreferrer noopener"
 				   title="Creative Commons License: Attribution-NonCommercial-ShareAlike"
 				   target="_blank"
 				   href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
@@ -46,13 +46,13 @@ global $site_url, $site_name; ?>
 			</a>
 		</div>
 		<div class="navbar-nav col-xl-1 col-md-2 d-inline text-end">
-			<a title="Back to top" class="nav-link d-inline p-0" href="#top">
+			<a title="Back to top" class="nav-link d-inline p-0 footer-hover" href="#top">
 				<svg style="width:20px;height:20px" aria-label="Arrow up" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path></svg>
 			</a>
-			<a rel="noreferrer noopener" title="Follow me on Twitter" class="nav-link d-inline ms-2 p-0" target="_blank" href="https://twitter.com/xlthlx/">
+			<a rel="noreferrer noopener" title="Follow me on Twitter" class="nav-link d-inline ms-2 p-0 footer-hover" target="_blank" href="https://twitter.com/xlthlx/">
 				<svg style="width:20px;height:20px" aria-label="Twitter" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg>
 			</a>
-			<a title="Feed RSS" class="nav-link d-inline ms-2 p-0" target="_blank" href="<?php echo $site_url; ?>feed/">
+			<a title="Feed RSS" class="nav-link d-inline ms-2 p-0 footer-hover" target="_blank" href="<?php echo $site_url; ?>feed/">
 				<svg style="width:20px;height:20px" aria-label="Feed RSS" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M128.081 415.959c0 35.369-28.672 64.041-64.041 64.041S0 451.328 0 415.959s28.672-64.041 64.041-64.041 64.04 28.673 64.04 64.041zm175.66 47.25c-8.354-154.6-132.185-278.587-286.95-286.95C7.656 175.765 0 183.105 0 192.253v48.069c0 8.415 6.49 15.472 14.887 16.018 111.832 7.284 201.473 96.702 208.772 208.772.547 8.397 7.604 14.887 16.018 14.887h48.069c9.149.001 16.489-7.655 15.995-16.79zm144.249.288C439.596 229.677 251.465 40.445 16.503 32.01 7.473 31.686 0 38.981 0 48.016v48.068c0 8.625 6.835 15.645 15.453 15.999 191.179 7.839 344.627 161.316 352.465 352.465.353 8.618 7.373 15.453 15.999 15.453h48.068c9.034-.001 16.329-7.474 16.005-16.504z"></path></svg>
 			</a>
 		</div>
