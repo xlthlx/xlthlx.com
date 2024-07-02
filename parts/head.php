f6fcbc2b568eba601b371a97c4d909bb9b266562
xlthlx <xlthlx@gmail.com>
2024-07-02 09:16:19 +0100

    Fix header

--- parts/head.php
+++ parts/head.php
@@ -7,12 +7,10 @@
 
 global $charset; ?>
 <head>
-    <meta charset="<?php echo $charset; ?>">
-    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="theme-color" content="#CACBE2"/>
-	<?php // @codingStandardsIgnoreStart ?>
-    <link rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt"/>
-	<?php // @codingStandardsIgnoreEnd ?>
+	<meta charset="<?php echo $charset; ?>">
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="theme-color" content="#CACBE2"/>
+	<link rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt"/>
 	<?php wp_head(); ?>
 </head>
