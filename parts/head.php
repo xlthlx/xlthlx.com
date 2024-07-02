a1055787e3c59077406b73d70c551f464cfb034f
xlthlx <xlthlx@gmail.com>
2024-07-02 09:08:11 +0100

    Remove favicon

--- parts/head.php
+++ parts/head.php
@@ -7,13 +7,12 @@
 
 global $charset; ?>
 <head>
-<meta charset="<?php echo $charset; ?>">
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="theme-color" content="#CACBE2"/>
-<?php // @codingStandardsIgnoreStart ?>
-<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" sizes="any">
-<link rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt"/>
-<?php // @codingStandardsIgnoreEnd ?>
-<?php wp_head(); ?>
+    <meta charset="<?php echo $charset; ?>">
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#CACBE2"/>
+	<?php // @codingStandardsIgnoreStart ?>
+    <link rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt"/>
+	<?php // @codingStandardsIgnoreEnd ?>
+	<?php wp_head(); ?>
 </head>
