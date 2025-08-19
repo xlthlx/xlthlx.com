b7c074813b2936d91cc2c7aba1978ad9be27a303
xlthlx <xlthlx@gmail.com>
2025-08-19 11:18:16 +0100

    Og image

--- parts/head.php
+++ parts/head.php
@@ -13,7 +13,7 @@
 function xlt_get_text() {
     global $post, $site_desc;
 
-    if ( ! is_home() && ! is_front_page() ) {
+    if ( ! is_home() && ! is_front_page() && ! is_archive() ) {
 
         if ( isset( $post->post_content ) && '' !== trim( $post->post_content ) ) {
             $post_object = get_post( $post );
@@ -35,13 +35,29 @@ function xlt_get_text() {
             }
 
             if ( '' === $text ) {
-                return $site_desc;
+                return html_entity_decode( $site_desc, ENT_HTML5 );
             }
 
             return $text;
         }
     } else {
-        return $site_desc;
+        return html_entity_decode( $site_desc, ENT_HTML5 );
+    }
+
+    return '';
+}
+
+/**
+ * Gets the og:image from the post thumbnail.
+ *
+ * @return false|string
+ */
+function xlt_get_og_img() {
+    global $post;
+
+    if ( ! is_home() && ! is_front_page() && ! is_archive() ) {
+        $post_object = get_post( $post );
+        return get_the_post_thumbnail_url( $post_object, 'full' );
     }
 
     return '';
@@ -53,6 +69,7 @@ global $charset; ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="fediverse:creator" content="@xlthlx@hachyderm.io">
     <meta property="og:description" name="description" content="<?php echo xlt_get_text(); ?>">
+    <meta property="og:image" content="<?php echo esc_url( xlt_get_og_img() ); ?>">
 	<link rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt"/>
     <link rel="canonical" href="<?php echo esc_attr( get_permalink() ); ?>">
 	<?php wp_head(); ?>
