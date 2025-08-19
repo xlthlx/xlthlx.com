6f8d676e4645fc2debc024dd37d956fdccbff7e7
xlthlx <xlthlx@gmail.com>
2025-08-19 08:36:19 +0100

    Og descrition

--- parts/head.php
+++ parts/head.php
@@ -5,13 +5,52 @@
  * @package  xlthlx
  */
 
+/**
+ * Get the description from the post content.
+ *
+ * @return string
+ */
+function xlt_get_text() {
+    global $post;
+
+    if ( isset( $post->post_content ) && '' !== trim( $post->post_content ) ) {
+        $post_desc = get_post( $post );
+        $text      = get_the_content( '', false, $post_desc );
+
+        $text = strip_shortcodes( $text );
+        $text = excerpt_remove_blocks( $text );
+        $text = excerpt_remove_footnotes( $text );
+
+        $filter_image_removed = remove_filter( 'the_content', 'wp_filter_content_tags', 12 );
+        $filter_block_removed = remove_filter( 'the_content', 'do_blocks', 9 );
+
+        $text = apply_filters( 'the_content', $text );
+        $text = str_replace( ']]>', ']]&gt;', $text );
+
+        if ( $filter_block_removed ) {
+            add_filter( 'the_content', 'do_blocks', 9 );
+        }
+        if ( $filter_image_removed ) {
+            add_filter( 'the_content', 'wp_filter_content_tags', 12 );
+        }
+
+        if ( strlen( $text ) > 200 ) {
+            $text = substr( $text, 0, 200 );
+        }
+
+        return $text;
+    }
+
+    return '';
+}
+
 global $charset; ?>
 <head>
 	<meta charset="<?php echo $charset; ?>">
-	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="theme-color" content="#0A0A11"/>
     <meta name="fediverse:creator" content="@xlthlx@hachyderm.io">
+    <meta property="og:description" name="description" content="<?php echo esc_attr( xlt_get_text() ); ?>">
 	<link rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt"/>
+    <link rel="canonical" href="<?php echo esc_attr( get_permalink() ); ?>">
 	<?php wp_head(); ?>
 </head>
