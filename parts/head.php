710825814605e6c4bb2ad6e279382951e80b432e
xlthlx <xlthlx@gmail.com>
2025-08-19 09:56:21 +0100

    Desc again

--- parts/head.php
+++ parts/head.php
@@ -13,24 +13,30 @@
 function xlt_get_text() {
     global $post;
 
-    if ( isset( $post->post_content ) && '' !== trim( $post->post_content ) ) {
-        $post_object = get_post( $post );
-        $text      = get_the_content( '', false, $post_object );
+    if ( ! is_home() && ! is_front_page() ) {
 
-        $text = strip_shortcodes( $text );
-        $text = excerpt_remove_blocks( $text );
-        $text = excerpt_remove_footnotes( $text );
+        if ( isset( $post->post_content ) && '' !== trim( $post->post_content ) ) {
+            $post_object = get_post( $post );
+            $text        = get_the_content( '', false, $post_object );
 
-        $text = apply_filters( 'the_excerpt', $text );
-        $text = str_replace( ']]>', ']]&gt;', $text );
-        $text = wp_strip_all_tags( $text );
-        $text = preg_replace("/<br>|<br \/>/", " ", $text);
+            $text = strip_shortcodes( $text );
+            $text = excerpt_remove_blocks( $text );
+            $text = excerpt_remove_footnotes( $text );
 
-        if ( strlen( $text ) > 200 ) {
-            $text = substr( $text, 0, 200 );
+            $text = apply_filters( 'the_excerpt', $text );
+            $text = str_replace( ']]>', ']]&gt;', $text );
+            $text = wp_strip_all_tags( $text );
+
+            if ( strlen( $text ) > 200 ) {
+                $text = substr( $text, 0, 200 );
+            }
+
+            return $text;
         }
+    } else {
+        global $site_desc;
 
-        return $text;
+        return $site_desc;
     }
 
     return '';
@@ -38,11 +44,11 @@ function xlt_get_text() {
 
 global $charset; ?>
 <head>
-	<meta charset="<?php echo $charset; ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="<?php echo $charset; ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="fediverse:creator" content="@xlthlx@hachyderm.io">
     <meta property="og:description" name="description" content="<?php echo xlt_get_text(); ?>">
-	<link rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt"/>
+    <link rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt"/>
     <link rel="canonical" href="<?php echo esc_attr( get_permalink() ); ?>">
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
